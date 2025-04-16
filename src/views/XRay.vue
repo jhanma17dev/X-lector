301988c5f95019f45c1946e4422f7af6ee167cf4
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-04-16 13:22:11 -0500

    Feat: X-ray view

--- src/views/XRay.vue
+++ src/views/XRay.vue
@@ -1,22 +1,180 @@
 <template>
   <span class="text-2xl font-semibold pl-4">X-Ray Diagnosis</span>
 
-  <div
-    class="w-full full-height outline-dashed outline-primary mt-6 rounded-2xl bg-primary/10 flex flex-col items-center justify-center text-sm font-semibold"
-  >
-    <span>Add an X-Rey file</span>
+  <div v-if="!file"
+    class="w-full full-height outline-dashed outline-primary mt-6 rounded-2xl bg-primary/10 flex flex-col items-center justify-center text-sm font-semibold">
+    <span>Add an X-Ray file</span>
     <span class="mt-1">Allowed Formats: DICOM, PDF, PNG, JPEG</span>
-    <button class="btn btn-primary rounded-full mt-4 text-sm">
+    <button class="btn btn-primary rounded-full mt-4 text-sm" @click="handleFileUpload">
       <span class="material-symbols-outlined"> add </span>
       Add File
+      <input id="file-upload" type="file" class="hidden" accept=".dcm,.pdf,.png,.jpg,.jpeg" />
     </button>
+  </div>
+
+  <div v-else class="w-full full-height mt-6 flex overflow-scroll no-scrollbar">
+    <div class="w-4/10 min-w-4/10 mr-8 rounded-2xl sticky top-0">
+      <!-- Display file according to type -->
+      <div v-if="fileType === 'image'" class="w-full h-full">
+        <img :src="filePreview" class="w-full h-full object-fit rounded-2xl" alt="X-Ray Image" />
+      </div>
+      <div v-else-if="fileType === 'pdf'" class="w-full h-full">
+        <iframe :src="filePreview" class="w-full h-full rounded-2xl" title="PDF Document"></iframe>
+      </div>
+    </div>
+
+    <div class="relative flex flex-col">
+      <div class="mask-opacity chat overflow-scroll no-scrollbar">
+        <div class="w-full bg-base-300 rounded-2xl flex flex-col border border-neutral mb-6">
+          <span class="text-xl font-bold p-4">Conclusions</span>
+
+          <hr class="border-neutral" />
+
+          <div class="text-sm p-4">
+            <!--List of conclusions-->
+            <ul class="list-disc list-outside pl-4">
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores distinctio recusandae
+                beatae nemo laboriosam molestiae quasi?
+                <span class="underline bg-error/50 text-error-content">Commodi quo quos itaque</span>
+                natus sit ipsum dolorum temporibus error. Optio excepturi quas quod!
+              </li>
+              <br />
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <span class="underline bg-warning/50 text-warning-content">Maiores distinctio recusandae beatae</span>
+                nemo laboriosam molestiae quasi? Commodi quo quos itaque natus sit ipsum dolorum
+                temporibus error. Optio excepturi quas quod!
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="w-full bg-base-300 rounded-2xl flex flex-col border border-neutral mb-6">
+          <span class="text-xl font-bold p-4">Suggestions</span>
+
+          <hr class="border-neutral" />
+
+          <div class="text-sm p-4">
+            <div class="flex">
+              <div
+                class="flex min-w-[64px] min-h-[64px] w-[64px] h-[64px] rounded-lg bg-primary/30 items-center justify-center relative">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px"
+                  fill="#7dc8c2">
+                  <path
+                    d="m826-585-42-42 45-45-157-157-45 45-43-43 30-31q23-23 57-22.5t57 23.5l129 129q23 23 23 56.5T857-615l-31 30ZM346-104q-23 23-56.5 23T233-104L90-247q-17-17.38-17-42.69T90-332l44-44 43 42-45 45 157 157 45-45 42 43-30 30Zm397-308 85-85-331-331-85 85 331 331ZM463-132l86-86-331-331-86 86 331 331Zm9-248 109-109-92-92-109 109 92 92Zm33 290q-16.93 17-41.97 17Q438-73 421-90L90-421q-17-16.93-17-41.97Q73-488 90-505l85-86q17.38-17 42.69-17T260-591l77 77 110-110-77-77q-17-16.93-17-41.97Q353-768 370-785l85-86q17.38-17 42.69-17T540-871l331 331q17 17.38 17 42.69T871-455l-86 85q-16.93 17-41.97 17Q718-353 701-370l-77-77-110 110 77 77q17 17.38 17 42.69T591-175l-86 85Z" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                  fill="#f59b08" class="absolute -top-2 -right-2 m-auto">
+                  <path
+                    d="m48-144 432-720 432 720H48Zm431.79-120q15.21 0 25.71-10.29t10.5-25.5q0-15.21-10.29-25.71t-25.5-10.5q-15.21 0-25.71 10.29t-10.5 25.5q0 15.21 10.29 25.71t25.5 10.5ZM444-384h72v-192h-72v192Z" />
+                </svg>
+              </div>
+
+              <div class="flex flex-col ml-4">
+                <span class="text-sm font-semibold">Lorem ipsum dolor sit amet</span>
+                <span class="text-xs text-neutral mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Delectus vel reprehenderit
+                  accusantium ad corporis! Sunt fugiat voluptatem quisquam molestiae quas temporibus
+                  animi eius voluptates modi consectetur illum itaque, reprehenderit cumque?</span>
+              </div>
+            </div>
+
+            <div class="flex mt-8">
+              <div
+                class="flex min-w-[64px] min-h-[64px] w-[64px] h-[64px] rounded-lg bg-primary/30 items-center justify-center relative">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px"
+                  fill="#7dc8c2">
+                  <path
+                    d="M533-96q-97.62 0-166.31-68.98Q298-233.97 298-332v-30q-85-11-143.5-74.5T96-588v-228h120v-48h72v168h-72v-48h-48v156.46q0 64.54 45.5 110.04T324-432q65 0 110.5-45.5T480-587.54V-744h-48v48h-72v-168h72v48h120v228q0 84.35-51.5 146.67Q449-379 370-364v33q0 67.92 47.5 115.46Q465-168 533-167q68-1 115.5-48.54T696-331v-59.37Q659-401 635.5-432T612-504q0-50 35-85t85-35q50 0 85 35t35 85q0 41-23.5 72T768-390v58q0 97.62-69 166.31T533-96Z" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                  fill="#f42423" class="absolute -top-2 -right-2 m-auto">
+                  <path
+                    d="M479.79-300q15.21 0 25.71-10.29t10.5-25.5q0-15.21-10.29-25.71t-25.5-10.5q-15.21 0-25.71 10.29t-10.5 25.5q0 15.21 10.29 25.71t25.5 10.5ZM444-432h72v-240h-72v240ZM341-144 144-342v-277l197-197h278l197 197v278L618-144H341Z" />
+                </svg>
+              </div>
+
+              <div class="flex flex-col ml-4">
+                <span class="text-sm font-semibold">Lorem ipsum dolor sit amet</span>
+                <span class="text-xs text-neutral mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Delectus vel reprehenderit
+                  accusantium ad corporis! Sunt fugiat voluptatem quisquam molestiae quas temporibus
+                  animi eius voluptates modi consectetur illum itaque, reprehenderit cumque?</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <input class="w-full bg-base-300 rounded-2xl flex flex-col border border-neutral h-[40px] px-4">
+      </input>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const file = ref<File | null>(null)
+const filePreview = ref<string>('')
+
+// Determine file type for display purposes
+const fileType = computed(() => {
+  if (!file.value) return null
+
+  if (file.value.type.includes('image')) {
+    return 'image'
+  } else if (file.value.type.includes('pdf')) {
+    return 'pdf'
+  } else {
+    return 'other'
+  }
+})
+
+const handleFileUpload = () => {
+  const fileInput = document.getElementById('file-upload') as HTMLInputElement
+
+  fileInput.onchange = () => {
+    const selectedFile = fileInput.files?.[0]
+    if (selectedFile) {
+      file.value = selectedFile
+      filePreview.value = URL.createObjectURL(selectedFile)
+      console.log('Selected file:', file.value)
+    }
+  }
+
+  fileInput.click()
+}
+</script>
 
 <style scoped>
 .full-height {
   height: calc(100vh - 4rem - 160px);
+}
+
+.no-scrollbar {
+  scrollbar-width: none;
+  /* Firefox */
+  -ms-overflow-style: none;
+  /* Internet Explorer and Edge */
+}
+
+&::-webkit-scrollbar {
+  display: none;
+  /* Safari and Chrome */
+}
+
+.chat {
+  height: calc(100vh - 4rem - 200px);
+}
+
+.mask-opacity {
+  -webkit-mask-image: linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 90%, rgba(0,0,0,0));
+  -webkit-mask-size: 100%;
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-position: left top;
 }
 </style>
